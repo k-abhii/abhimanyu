1a6bb3c7f62675a6af4e31a5a1dfb211323db455
Abhimanyu_Kumar <kabhimanyu656@gmail.com>
2022-03-08 21:56:17 +0530

    object intro in js

---
+++ object.js
@@ -0,0 +1,10 @@
+// Empty object
+let obj ={};
+console.log(obj);
+objAbhimanyu={
+    "Name":" Abhimanyu Kumar",
+    Age : 27,
+    "Phone no ": 8860503744,
+    "LastName ": "Kumar"
+};
+console.log(objAbhimanyu);
