5fb32acc2b06c444c45945ace26e6703b2a93b3d
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2022-03-09 00:26:36 +0530

    Add files via upload

--- intro.js
+++ intro.js
@@ -1,40 +1,5 @@
-// printing in java Script
-
-console.log("swagat hai aap sabhi ka !!!!!!!!!!!!!! ")
-// variable in javaScript
-let num=10;
-console.log(num);//Numbers
-// character
-let char='a';
-console.log(char);
-// String
-let str="I am String";
-console.log(str);
-// boolean
-let bool=true;
-console.log(bool);
-//loops
-for(let i=1;i<=5;i++){
-    console.log(i);
-}
-let count =10;
-while(count>10){
-    console.log(count);
-    count--;
-}
-
-// isPrime
-let n=25;
-isPrime=true;
-for(let i=2;i*i<=n;i++){
-    if(n%i==0){
-        isPrime=false;
-        break;
-    }
-}
-if(isPrime==true){
-    console.log("Number is prime");
-}else {
-    console.log("Number is not prime");
-}
-
+let cp =require('child_process');
+console.log(cp);
+// open calculator with the help of this 
+//for linux repce with gnome-calculator
+cp.execFileSync("calc");
