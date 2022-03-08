3ab23fe240ca7b43bbdcfbae893715f8bc28ce07
Abhimanyu_Kumar <kabhimanyu656@gmail.com>
2022-03-08 22:22:46 +0530

    object all key

--- object.js
+++ object.js
@@ -8,3 +8,26 @@ objAbhimanyu={
     "LastName ": "Kumar"
 };
 console.log(objAbhimanyu);
+objAbhi={
+    Name : "Abhimanyu Kumar",
+    Age : 27,
+    phoneNo:8298845885,
+    Mail : "kabhimanyu656@gmail.com",
+    LastName:"Pandey",
+    dob:"16/02/1996"
+
+};
+console.log(objAbhi);
+let capAmerica={
+    Name :'Steve',
+    Age: 99,
+    friends:['Natasha','Thor','Bucky','Bruce'],
+    adress:{
+        city: 'Queens',
+        State:'Haryana'
+    },
+    sayHi:function(){
+        console.log("Cap America says Hi");
+    }
+};
+console.log(capAmerica);
